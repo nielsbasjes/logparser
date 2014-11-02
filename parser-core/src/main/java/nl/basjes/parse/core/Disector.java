c5e8e58e81043da6e4a3a3a8fe239427047c24b0
Niels Basjes <niels@basjes.nl>
2014-11-03 00:20:00 +0100

    Some more documentation

--- parser-core/src/main/java/nl/basjes/parse/core/Disector.java
+++ parser-core/src/main/java/nl/basjes/parse/core/Disector.java
@@ -25,6 +25,39 @@ import org.slf4j.LoggerFactory;
 import java.lang.reflect.Constructor;
 import java.util.List;
 
+/**
+ * <p>A Disector is a class capable of chopping a String into multiple values of specific types.</p>
+ * <p>The lifecycle:</p>
+ * <p><b>Parser setup</b></p>
+ * <ol>
+ * <li>First instance is constructed</li>
+ * <li>First instance is added to the {@link nl.basjes.parse.core.Parser} using
+ * {@link nl.basjes.parse.core.Parser#addDisector(Disector)} </li>
+ * <li>The {@link nl.basjes.parse.core.Parser} calls {@link #getInputType()} and {@link #getPossibleOutput()} to know
+ * what this disector can deliver.</li>
+ * </ol>
+ * <p>The parser now constructs a tree based on the available {@link nl.basjes.parse.core.Disector}s
+ * and what was requested.</p>
+ * <p><b>Disectors setup</b></p>
+ * <p>For each node in the tree a new instance of the required disector is created by calling {@link #getNewInstance()}
+ * which calls {@link #initializeNewInstance(Disector)}. Note that only {@link nl.basjes.parse.core.Disector}s
+ * that are acually needed will be in the parse tree.</p>
+ * <p>For each of those instances in the tree:</p>
+ * <ol>
+ * <li>For each of the actually needed input+output combinations {@link #prepareForDisect(String, String)} is called.
+ * This can be used to avoid needless CPU cycles during the actual run.</li>
+ * <li>As a final step a call to {@link #prepareForRun()} is done as an indication that all preparation input has been
+ * provided. A Disector can use this to finalize the runtime data structures so doing the actual disecting faster.</li>
+ * </ol>
+ * <p><b>Disecting</b></p>
+ * <ul>
+ * <li>During a run the instance will be called with {@link #disect} many times.</li>
+ * <li>In the {@link #disect(Parsable, String)} the actual value to be worked on must be retrieved using
+ * {@link nl.basjes.parse.core.Parsable#getParsableField(String, String)}</li>
+ * <li>The result(s) of the disection must be put back using
+ * {@link nl.basjes.parse.core.Parsable#addDisection(String, String, String, String, java.util.EnumSet)}</li>
+ * </ul>
+ */
 public abstract class Disector {
 
     private static final Logger LOG = LoggerFactory.getLogger(Disector.class);
@@ -74,6 +107,12 @@ public abstract class Disector {
 
     // --------------------------------------------
 
+    /**
+     * Create an additional instance of this disector.
+     * This is needed because in the parse tree we may need the same disector multiple times.
+     * In order to optimize per node we need separate instances.
+     * @return New instance of this Disector
+     */
     public Disector getNewInstance() {
         try {
             Constructor<? extends Disector> co = this.getClass().getConstructor();
