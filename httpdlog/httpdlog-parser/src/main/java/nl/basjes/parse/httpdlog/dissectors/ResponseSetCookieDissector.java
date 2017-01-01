dc0eab7a5888d1640ae8cd1536e9698bd490f552
Niels Basjes <niels@basjes.nl>
2017-01-01 21:20:03 +0100

    Rewrote parsing set-cookies

--- httpdlog/httpdlog-parser/src/main/java/nl/basjes/parse/httpdlog/dissectors/ResponseSetCookieDissector.java
+++ httpdlog/httpdlog-parser/src/main/java/nl/basjes/parse/httpdlog/dissectors/ResponseSetCookieDissector.java
@@ -21,8 +21,10 @@ import nl.basjes.parse.core.Dissector;
 import nl.basjes.parse.core.Parsable;
 import nl.basjes.parse.core.ParsedField;
 import nl.basjes.parse.core.exceptions.DissectionFailure;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
-import java.net.HttpCookie;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
@@ -45,6 +47,7 @@ public class ResponseSetCookieDissector extends Dissector {
         List<String> result = new ArrayList<>();
         result.add("STRING:value");
         result.add("STRING:expires");
+        result.add("TIME.EPOCH:expires");
         result.add("STRING:path");
         result.add("STRING:domain");
         return result;
@@ -67,8 +70,14 @@ public class ResponseSetCookieDissector extends Dissector {
 
     @Override
     public EnumSet<Casts> prepareForDissect(final String inputname, final String outputname) {
-        // We do not do anything extra here
-        return Casts.STRING_ONLY;
+        String name = extractFieldName(inputname, outputname);
+        switch (name) {
+            case "value":   return Casts.STRING_ONLY;
+            case "expires": return Casts.STRING_OR_LONG;
+            case "path":    return Casts.STRING_ONLY;
+            case "domain":  return Casts.STRING_ONLY;
+            default:        return Casts.STRING_ONLY;
+        }
     }
 
     // --------------------------------------------
@@ -89,30 +98,72 @@ public class ResponseSetCookieDissector extends Dissector {
             return; // Nothing to do here
         }
 
-        Long nowSeconds = System.currentTimeMillis()/1000;
-        List<HttpCookie> cookies = HttpCookie.parse(fieldValue);
+        String[] parts = fieldValue.split(";");
 
-        for (HttpCookie cookie : cookies) {
-            parsable.addDissection(inputname, getDissectionType(inputname, "value"), "value", cookie.getValue());
-            parsable.addDissection(inputname, getDissectionType(inputname, "expires"), "expires",
-                    Long.toString(nowSeconds + cookie.getMaxAge()));
-            parsable.addDissection(inputname, getDissectionType(inputname, "path"), "path", cookie.getPath());
-            parsable.addDissection(inputname, getDissectionType(inputname, "domain"), "domain", cookie.getDomain());
-            parsable.addDissection(inputname, getDissectionType(inputname, "comment"), "comment", cookie.getComment());
+        Long expires    = null;
+        String domain   = null;
+        String comment  = null;
+        String path     = null;
+
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i].trim();
+            String[] keyValue = part.split("=", 2);
+
+            String key = keyValue[0].trim();
+            String value = "";
+            if (keyValue.length == 2) {
+                value = keyValue[1].trim();
+            }
+
+            if (i==0) {
+                parsable.addDissection(inputname, "STRING", "value", value);
+            } else {
+                switch (key) {
+                    // We ignore the max-age field because that is unsupported by IE anyway.
+                    case "expires":
+                        expires = parseExpire(value);
+                        break;
+                    case "domain":
+                        domain = value;
+                        break;
+                    case "comment":
+                        comment = value;
+                        break;
+                    case "path":
+                        path = value;
+                        break;
+                    default: // Ignore anything else
+                }
+
+            }
         }
+
+        // Backwards compatibility: STRING version is in seconds
+        parsable.addDissection(inputname, "STRING",     "expires", expires == null ? null : expires / 1000);
+        parsable.addDissection(inputname, "TIME.EPOCH", "expires", expires);
+
+        parsable.addDissection(inputname, "STRING", "domain",   domain);
+        parsable.addDissection(inputname, "STRING", "comment",  comment);
+        parsable.addDissection(inputname, "STRING", "path",     path);
     }
 
     // --------------------------------------------
 
-    /**
-     * This determines the type of the value that was just found.
-     * This method is intended to be overruled by a subclass
-     */
-    @SuppressWarnings("UnusedParameters")
-    public String getDissectionType(final String basename, final String name) {
-        return "STRING"; // Possible outputs are of the same type.
-    }
+    private static final DateTimeFormatter[] DATE_FORMATS = {
+        DateTimeFormat.forPattern("EEE',' dd-MMM-yyyy HH:mm:ss 'GMT'").withZone(DateTimeZone.UTC),
+        DateTimeFormat.forPattern("EEE',' dd MMM yyyy HH:mm:ss 'GMT'").withZone(DateTimeZone.UTC),
+        DateTimeFormat.forPattern("EEE MMM dd yyyy HH:mm:ss 'GMT'Z")  .withZone(DateTimeZone.UTC),
+    };
 
-    // --------------------------------------------
+    private Long parseExpire(String expireString) {
+        for (DateTimeFormatter dateFormat: DATE_FORMATS) {
+            try {
+                return dateFormat.parseDateTime(expireString).getMillis();
+            } catch (IllegalArgumentException iae) {
+                // Ignore and continue
+            }
+        }
+        return 0L;
+    }
 
 }
