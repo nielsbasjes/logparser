26eeab8c99baf991b9ce7481ba82babfd3227aec
Niels Basjes <niels@basjes.nl>
2017-01-01 16:10:29 +0100

    Explicit test on query string dissecting

---
+++ httpdlog/httpdlog-parser/src/test/java/nl/basjes/parse/httpdlog/dissectors/TestQueryStringDissector.java
@@ -0,0 +1,45 @@
+/*
+ * Apache HTTPD logparsing made easy
+ * Copyright (C) 2011-2017 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.basjes.parse.httpdlog.dissectors;
+
+import nl.basjes.parse.core.nl.basjes.parse.core.test.DissectorTester;
+import org.junit.Test;
+
+public class TestQueryStringDissector {
+
+    @Test
+    public void testQueryString() throws Exception {
+        DissectorTester.create()
+            .withDissector(new HttpUriDissector())
+            .withDissector(new QueryStringFieldDissector())
+
+            .withInput("/some/thing/else/index.html&aap=1&noot=&mies&")
+
+            .expect("HTTP.PATH:path",            "/some/thing/else/index.html")
+            .expect("HTTP.QUERYSTRING:query",    "&aap=1&noot=&mies&")
+            .expect("STRING:query.aap",          "1")           // Present with value
+            .expect("STRING:query.noot",         "")            // Present without value
+            .expect("STRING:query.mies",         "")            // Present without value
+            .expect("STRING:query.wim",          (String)null)  // NOT Present
+
+            .checkExpectations();
+    }
+
+
+
+}
