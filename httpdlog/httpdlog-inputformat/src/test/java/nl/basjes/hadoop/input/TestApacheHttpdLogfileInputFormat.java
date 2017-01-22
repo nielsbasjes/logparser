742bf21d6d723d7fe53b3dc6e4c1f2f33118a4e4
Niels Basjes <niels@basjes.nl>
2017-01-22 14:11:56 +0100

    Do all outputs test for InputFormat wrapper

--- httpdlog/httpdlog-inputformat/src/test/java/nl/basjes/hadoop/input/TestApacheHttpdLogfileInputFormat.java
+++ httpdlog/httpdlog-inputformat/src/test/java/nl/basjes/hadoop/input/TestApacheHttpdLogfileInputFormat.java
@@ -16,6 +16,9 @@ package nl.basjes.hadoop.input;
  * limitations under the License.
  */
 
+import nl.basjes.parse.core.Dissector;
+import nl.basjes.parse.core.test.UltimateDummyDissector;
+import nl.basjes.parse.httpdlog.HttpdLogFormatDissector;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.InputFormat;
@@ -29,18 +32,26 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class TestApacheHttpdLogfileInputFormat {
+    // CHECKSTYLE.OFF: LineLength
+    String logformat = "%h %l %u %t \"%r\" %>s %O \"%{%D %F %R %T %r %a %A %b %B %C %d %G %h %H %I %j %k %l %m %M %p %S %u %Y %z}t\" \"%{User-Agent}i\"";
+    // CHECKSTYLE.ON: LineLength
+
     @Test
     public void checkInputFormat() throws IOException, InterruptedException {
-        // CHECKSTYLE.OFF: LineLength
-        String logformat = "%h %l %u %t \"%r\" %>s %O \"%{%D %F %R %T %r %a %A %b %B %C %d %G %h %H %I %j %k %l %m %M %p %S %u %Y %z}t\" \"%{User-Agent}i\"";
-        // CHECKSTYLE.ON: LineLength
-
         Configuration conf = new Configuration(false);
         conf.set("fs.default.name", "file:///");
 
@@ -67,6 +78,78 @@ public class TestApacheHttpdLogfileInputFormat {
         if (value instanceof ParsedRecord) {
             assertEquals("1483272081000", ((ParsedRecord) value).getString("TIME.EPOCH:request.receive.time.epoch"));
             assertEquals("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36", ((ParsedRecord) value).getString("HTTP.USERAGENT:request.user-agent"));
+        } else {
+            fail("Wrong return class type");
+        }
+    }
+
+    @Test
+    public void checkAllOutputTypes() throws IOException, InterruptedException {
+        Configuration conf = new Configuration(false);
+        conf.set("fs.default.name", "file:///");
+
+        // A ',' separated list of fields
+        List<String> fields = Arrays.asList(
+                "ANY:any" ,
+                "ANY:any" ,
+                "ANY:any" ,
+                "STRING:string" ,
+                "STRING:string" ,
+                "STRING:string" ,
+                "INT:int" ,
+                "INT:int" ,
+                "INT:int" ,
+                "LONG:long" ,
+                "LONG:long" ,
+                "LONG:long" ,
+                "FLOAT:float" ,
+                "FLOAT:float" ,
+                "FLOAT:float" ,
+                "DOUBLE:double" ,
+                "DOUBLE:double" ,
+                "DOUBLE:double");
+
+        File testFile = new File("src/test/resources/access.log");
+        Path path = new Path(testFile.getAbsoluteFile().toURI());
+        FileSplit split = new FileSplit(path, 0, testFile.length(), null);
+
+        Map<String, Set<String>> typeRemappings = new HashMap<>();
+        List<Dissector> dissectors = new ArrayList<>();
+        dissectors.add(new UltimateDummyDissector(HttpdLogFormatDissector.INPUT_TYPE));
+
+        InputFormat inputFormat = new ApacheHttpdLogfileInputFormat(
+            logformat,
+            fields,
+            typeRemappings,
+            dissectors);
+        TaskAttemptContext context = new TaskAttemptContextImpl(conf, new TaskAttemptID());
+        RecordReader reader = inputFormat.createRecordReader(split, context);
+
+        reader.initialize(split, context);
+
+        assertTrue(reader.nextKeyValue());
+
+        Object value = reader.getCurrentValue();
+        if (value instanceof ParsedRecord) {
+            ParsedRecord record = (ParsedRecord)value;
+            assertEquals("42",          record.getString("ANY:any"));             // any_string
+            assertEquals(42L,           record.getLong("ANY:any").longValue());   // any_long
+            assertEquals(42D,           record.getDouble("ANY:any") ,0.1D);       // any_double
+            assertEquals("42",          record.getString("STRING:string"));       // string_string
+            assertEquals(null,          record.getLong("STRING:string")  );       // string_long
+            assertEquals(null,          record.getDouble("STRING:string"));       // string_double
+            assertEquals("42",          record.getString("INT:int"));             // int_string
+            assertEquals(42L,           record.getLong("INT:int").longValue());   // int_long
+            assertEquals(null,          record.getDouble("INT:int"));             // int_double
+            assertEquals("42",          record.getString("LONG:long"));           // long_string
+            assertEquals(42L,           record.getLong("LONG:long").longValue()); // long_long
+            assertEquals(null,          record.getDouble("LONG:long"));           // long_double
+            assertEquals("42.0",        record.getString("FLOAT:float"));         // float_string
+            assertEquals(null,          record.getLong("FLOAT:float")  );         // float_long
+            assertEquals(42D,           record.getDouble("FLOAT:float") ,0.1D);   // float_double
+            assertEquals("42.0",        record.getString("DOUBLE:double"));       // double_string
+            assertEquals(null,          record.getLong("DOUBLE:double")  );       // double_long
+            assertEquals(42D,           record.getDouble("DOUBLE:double") ,0.1D);  // double_double
         } else {
             fail("Wrong return class type");
         }
