fcb6245eb23b72fa6192074d00d65b11de9594aa
Niels Basjes <niels@basjes.nl>
2018-03-13 10:52:56 +0100

    Improved testing of the parser core

---
+++ parser-core/src/test/java/nl/basjes/parse/core/ParserDuplicateOutputTest.java
@@ -0,0 +1,65 @@
+/*
+ * Apache HTTPD & NGINX Access log parsing made easy
+ * Copyright (C) 2011-2018 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.basjes.parse.core;
+
+import nl.basjes.parse.core.exceptions.DissectionFailure;
+import nl.basjes.parse.core.test.DissectorTester;
+import org.junit.Test;
+
+import java.util.EnumSet;
+import java.util.HashMap;
+
+public class ParserDuplicateOutputTest {
+
+    public abstract static class TestDissector extends SimpleDissector {
+        private static HashMap<String, EnumSet<Casts>> dissectorConfig = new HashMap<>();
+        static {
+            dissectorConfig.put("STRING:output",   Casts.STRING_ONLY);
+        }
+
+        public TestDissector() {
+            super("INPUT", dissectorConfig);
+        }
+    }
+
+    public static class FooDissector extends TestDissector {
+        @Override
+        public void dissect(Parsable<?> parsable, String inputname, Value value) throws DissectionFailure {
+            parsable.addDissection(inputname, "STRING", "output", "foo");
+        }
+    }
+    public static class BarDissector extends TestDissector {
+        @Override
+        public void dissect(Parsable<?> parsable, String inputname, Value value) throws DissectionFailure {
+            parsable.addDissection(inputname, "STRING", "output", "bar");
+        }
+    }
+
+    // Verify: If you have two dissectors doing the SAME input/output you should get BOTH
+    @Test
+    public void testParseString() {
+        DissectorTester.create()
+            .verbose()
+            .withDissector(new FooDissector())
+            .withDissector(new BarDissector())
+            .withInput("SomeThing")
+            .printPossible()
+            .expect("STRING:output", "foo")
+            .expect("STRING:output", "bar")
+            .checkExpectations();
+    }
+}
--- parser-core/src/test/java/nl/basjes/parse/core/test/TestRecord.java
+++ parser-core/src/test/java/nl/basjes/parse/core/test/TestRecord.java
@@ -20,10 +20,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -31,9 +34,9 @@ public class TestRecord {
 
     private static final Logger LOG = LoggerFactory.getLogger(DissectorTester.class);
 
-    private final Map<String, String> stringMap = new HashMap<>(32);
-    private final Map<String, Long> longMap = new HashMap<>(32);
-    private final Map<String, Double> doubleMap = new HashMap<>(32);
+    private final Map<String, Set<String>>  stringMap   = new HashMap<>(32);
+    private final Map<String, Set<Long>>    longMap     = new HashMap<>(32);
+    private final Map<String, Set<Double>>  doubleMap   = new HashMap<>(32);
 
     public void setVerbose() {
         this.verbose = true;
@@ -45,53 +48,101 @@ public class TestRecord {
         if (verbose) {
             LOG.info("Received String: {} = {}", name, value);
         }
-        stringMap.put(name, value);
+        stringMap.computeIfAbsent(name, s -> new HashSet<>()).add(value);
     }
+
     public void setLongValue(final String name, final Long value) {
         if (verbose) {
             LOG.info("Received Long  : {} = {}", name, value);
         }
-        longMap.put(name, value);
+        longMap.computeIfAbsent(name, s -> new HashSet<>()).add(value);
     }
+
     public void setDoubleValue(final String name, final Double value) {
         if (verbose) {
             LOG.info("Received Double: {} = {}", name, value);
         }
-        doubleMap.put(name, value);
+        doubleMap.computeIfAbsent(name, s -> new HashSet<>()).add(value);
     }
 
     public String  getStringValue(final String name) {
+        Set<String> value = stringMap.get(name);
+        if (value == null) {
+            return null;
+        }
+        return value.iterator().next();
+    }
+
+    public Long    getLongValue(final String name) {
+        Set<Long> value = longMap.get(name);
+        if (value == null) {
+            return null;
+        }
+        return value.iterator().next();
+    }
+
+    public Double  getDoubleValue(final String name) {
+        Set<Double> value = doubleMap.get(name);
+        if (value == null) {
+            return null;
+        }
+        return value.iterator().next();
+    }
+
+    public Set<String>  getStringValues(final String name) {
         return stringMap.get(name);
     }
-    public Long    getLongValue(final String name) {
+
+    public Set<Long>    getLongValues(final String name) {
         return longMap.get(name);
     }
-    public Double  getDoubleValue(final String name) {
+
+    public Set<Double>  getDoubleValues(final String name) {
         return doubleMap.get(name);
     }
 
     public boolean hasStringValue(final String name) {
         return stringMap.containsKey(name);
     }
+
     public boolean hasLongValue(final String name) {
         return longMap.containsKey(name);
     }
+
     public boolean hasDoubleValue(final String name) {
         return doubleMap.containsKey(name);
     }
 
-    public TestRecord expectString(String field, String value) {
-        isPresent(stringMap, field, value);
+    public TestRecord expectString(String field, String... values) {
+        if (values == null){
+            isPresent(stringMap, field, values);
+        } else {
+            for (String value : values) {
+                isPresent(stringMap, field, value);
+            }
+        }
         return this;
     }
 
-    public TestRecord expectLong(String field, Long value) {
-        isPresent(longMap, field, value);
+    public TestRecord expectLong(String field, Long... values) {
+        if (values == null){
+            isPresent(longMap, field, values);
+        } else {
+            for (Long value : values) {
+                isPresent(longMap, field, value);
+            }
+        }
         return this;
     }
 
-    public TestRecord expectDouble(String field, Double value) {
-        isPresent(doubleMap, field, value);
+    public TestRecord expectDouble(String field, Double... values) {
+        if (values == null){
+            isPresent(doubleMap, field, values);
+        } else {
+            for (Double value : values) {
+                isPresent(doubleMap, field, value);
+            }
+        }
         return this;
     }
 
@@ -99,16 +150,22 @@ public class TestRecord {
         if (value == null) {
             assertTrue("The field \""+field+"\" is missing (a null value was expected).", results.containsKey(field));
             Object actualValue = results.get(field);
-            if (actualValue != null) {
-                assertEquals("The field \"" + field + "\" should be null but it was: " +
-                    "(" + actualValue.getClass().getSimpleName() + ")\"" + actualValue + "\" ", null, actualValue);
+            assertNotNull("The field \"" + field + "\" should be present but it is not", actualValue);
+            assertTrue("Invalid type used, result must be a Set<?>", actualValue instanceof Set);
+            Set<?> actualValues = (Set<?>)actualValue;
+            for (Object actualValuee: actualValues) {
+                assertEquals("The field \"" + field + "\" should only have null values but we found: " +
+                    "(" + actualValue.getClass().getSimpleName() + ")\"" + actualValue + "\" ", null, actualValuee);
             }
         } else {
             assertTrue("The field \""+field+"\" is missing (an entry of type "+value.getClass().getSimpleName()+" was expected).",
                 results.containsKey(field));
-            assertEquals("The field \"" + field + "\" should have the value (" +
+            Object result = results.get(field);
+            assertTrue("Invalid type used, result must be a Set<?>", result instanceof Set);
+            Set<?> resultSet = (Set<?>)result;
+            assertTrue("The field \"" + field + "\" should have the value (" +
                 value
-                + ")\"" + value.toString() + "\"is missing", value, results.get(field));
+                + ")\"" + value.toString() + "\"is missing", resultSet.contains(value));
         }
     }
 
@@ -132,7 +189,7 @@ public class TestRecord {
         if (value != null) {
             fail("The value \""+value+"\" was found for field \""+field+"\"");
         } else {
-            assertFalse("A null value was found for field \"" + field + "\"", results.containsKey(field));
+            assertFalse("A null value was found for field \"" + field + "\"", results.containsKey(field) || results.get(field) != null);
         }
     }
 
