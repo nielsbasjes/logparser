591b9cfe388ed066ba772e5ce6a83150a4d82316
Niels Basjes <niels@basjes.nl>
2018-07-09 22:03:01 +0200

    Improve docker build environment

--- start-docker.sh
+++ start-docker.sh
@@ -19,39 +19,69 @@
 OS=centos7
 PROJECTNAME=logparser
 CONTAINER_NAME=${PROJECTNAME}-${OS}-${USER}-$$
+DOCKER_BUILD="docker build"
 
-docker build -t ${PROJECTNAME}-${OS} docker/${OS}
+if [[ "$(docker images -q ${PROJECTNAME}-${OS} 2> /dev/null)" == "" ]]; then
+  DOCKER_BUILD="docker build"
+cat << "Welcome-message"
+
+ _____      _   _   _                                             _                                      _
+/  ___|    | | | | (_)                                           (_)                                    | |
+\ `--.  ___| |_| |_ _ _ __   __ _   _   _ _ __     ___ _ ____   ___ _ __ ___  _ __  _ __ ___   ___ _ __ | |_
+ `--. \/ _ \ __| __| | '_ \ / _` | | | | | '_ \   / _ \ '_ \ \ / / | '__/ _ \| '_ \| '_ ` _ \ / _ \ '_ \| __|
+/\__/ /  __/ |_| |_| | | | | (_| | | |_| | |_) | |  __/ | | \ V /| | | | (_) | | | | | | | | |  __/ | | | |_
+\____/ \___|\__|\__|_|_| |_|\__, |  \__,_| .__/   \___|_| |_|\_/ |_|_|  \___/|_| |_|_| |_| |_|\___|_| |_|\__|
+                             __/ |       | |
+                            |___/        |_|
+
+ For building Logparser project
+
+ This will take a few minutes...
+Welcome-message
+else
+  DOCKER_BUILD="docker build -q"
+  echo "Loading LogParser development environment"
+fi
+
+${DOCKER_BUILD} -t ${PROJECTNAME}-${OS} docker/${OS}
 
 if [ "$(uname -s)" == "Linux" ]; then
   USER_NAME=${SUDO_USER:=${USER}}
-  USER_ID=$(id -u ${USER_NAME})
-  GROUP_ID=$(id -g ${USER_NAME})
+  USER_ID=$(id -u "${USER_NAME}")
+  GROUP_ID=$(id -g "${USER_NAME}")
 else # boot2docker uid and gid
   USER_NAME=${USER}
   USER_ID=1000
   GROUP_ID=50
 fi
 
-docker build -t ${PROJECTNAME}-${OS}-${USER_NAME} - <<UserSpecificDocker
+${DOCKER_BUILD} -t "${PROJECTNAME}-${OS}-${USER_NAME}" - <<UserSpecificDocker
 FROM ${PROJECTNAME}-${OS}
-RUN groupadd -g ${GROUP_ID} ${USER_NAME} || true
-RUN useradd -g ${GROUP_ID} -u ${USER_ID} -k /root -m ${USER_NAME}
-ENV HOME /home/${USER_NAME}
+RUN bash /scripts/configure-for-user.sh "${USER_NAME}" "${USER_ID}" "${GROUP_ID}" "$(fgrep vboxsf /etc/group)"
 UserSpecificDocker
 
 # Do NOT Map the real ~/.m2 directory !!!
-[ -d ${PWD}/docker/_m2    ] || mkdir ${PWD}/docker/_m2
-[ -d ${PWD}/docker/_gnupg ] || mkdir ${PWD}/docker/_gnupg
+[ -d "${PWD}/docker/_m2"    ] || mkdir "${PWD}/docker/_m2"
+[ -d "${PWD}/docker/_gnupg" ] || mkdir "${PWD}/docker/_gnupg"
 
-docker run --rm=true -t -i                                    \
-           -u ${USER_NAME}                                    \
-           -v ${PWD}:/home/${USER_NAME}/${PROJECTNAME}        \
-           -v ${PWD}/docker/_m2:/home/${USER_NAME}/.m2        \
-           -v ${PWD}/docker/_gnupg:/home/${USER_NAME}/.gnupg  \
-           -w /home/${USER}/${PROJECTNAME}                    \
-           --name "${CONTAINER_NAME}"                         \
-           ${PROJECTNAME}-${OS}-${USER_NAME}                  \
+MOUNTGPGDIR="${PWD}/docker/_gnupg"
+
+if [[ "${1}" == "RELEASE" ]];
+then
+  cp "${HOME}"/.m2/*.xml "${PWD}/docker/_m2"
+  MOUNTGPGDIR="${HOME}/.gnupg"
+
+  echo "Setting up for release process"
+fi
+
+docker run --rm=true -t -i                                      \
+           -u "${USER_NAME}"                                    \
+           -v "${PWD}:/home/${USER_NAME}/${PROJECTNAME}"        \
+           -v "${PWD}/docker/_m2:/home/${USER_NAME}/.m2"        \
+           -v "${MOUNTGPGDIR}:/home/${USER_NAME}/.gnupg"  \
+           -w "/home/${USER}/${PROJECTNAME}"                    \
+           --name "${CONTAINER_NAME}"                           \
+           "${PROJECTNAME}-${OS}-${USER_NAME}"                  \
            bash
-
 
 exit 0
