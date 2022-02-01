e90c5a646001aefbcd7764bfc7304eae259c4dcf
Niels Basjes <niels@basjes.nl>
2022-02-01 16:43:47 +0100

    Updated the docker build environment

---
+++ devtools/docker/env.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Apache HTTPD & NGINX Access log parsing made easy
+# Copyright (C) 2011-2021 Niels Basjes
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+. "/scripts/build_env_checks.sh"
+. "/scripts/bashcolors.sh"
+
+export JDK_VERSION="???"
+
+function __INTERNAL__SwitchJDK {
+    JDK=$1
+    echo -e "${IRed}${On_Black}Setting JDK to version ${JDK}${Color_Off}"
+    sudo update-java-alternatives --set $(update-java-alternatives -l | fgrep "${JDK}" | sed 's@ \+@ @g' | cut -d' ' -f1);
+    export JAVA_HOME=$(update-java-alternatives -l | fgrep "${JDK}" | sed 's@ \+@ @g' | cut -d' ' -f3);
+#    export JDK_VERSION="JDK ${JDK}"
+}
+echo "Use switch-jdk8, switch-jdk11 or switch-jdk17 to select the desired JDK to build with."
+alias switch-jdk8="__INTERNAL__SwitchJDK 1.8.0; export JDK_VERSION=JDK-8"
+alias switch-jdk11="__INTERNAL__SwitchJDK 1.11.0; export JDK_VERSION=JDK-11"
+alias switch-jdk17="__INTERNAL__SwitchJDK 1.17.0 ; export JDK_VERSION=JDK-17"
+#alias switch-jdk13="__INTERNAL__SwitchJDK latest; export JDK_VERSION=JDK-13"
+
+switch-jdk11
+
+. "/usr/lib/git-core/git-sh-prompt"
+export PS1='\['${IBlue}${On_Black}'\] \u@\['${IWhite}${On_Red}'\][Yauaa Builder \['${BWhite}${On_Blue}'\]<'\${JDK_VERSION}'>\['${IWhite}${On_Red}'\]]\['${IBlue}${On_Black}'\]:\['${Cyan}${On_Black}'\]\w$(declare -F __git_ps1 &>/dev/null && __git_ps1 " \['${BIPurple}'\]{\['${BIGreen}'\]%s\['${BIPurple}'\]}")\['${BIBlue}'\] ]\['${Color_Off}'\]\n$ '
+
+alias documentation-serve="cd ~/yauaa/documentation && hugo server -b http://localhost --bind=0.0.0.0"
--- start-docker.sh
+++ start-docker.sh
@@ -16,13 +16,25 @@
 # limitations under the License.
 #
 
-OS=centos7
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+echo "PWD: ${SCRIPTDIR}"
+
+cd "${SCRIPTDIR}" || ( echo "This should not be possible" ; exit 1 )
+
 PROJECTNAME=logparser
-CONTAINER_NAME=${PROJECTNAME}-${OS}-${USER}-$$
+USER=$(id -un)
+CONTAINER_NAME=${PROJECTNAME}-devtools-${USER}-$$
 DOCKER_BUILD="docker build"
 
-if [[ "$(docker images -q ${PROJECTNAME}-${OS} 2> /dev/null)" == "" ]]; then
-  DOCKER_BUILD="docker build"
+if [ -n "${INSIDE_DOCKER+x}" ];
+then
+  echo "Nothing to do: You are already INSIDE the docker environment"
+  exit 1;
+fi
+
+if [[ "$(docker images -q ${PROJECTNAME}-devtools 2> /dev/null)" == "" ]]; then
+#  DOCKER_BUILD="docker build"
 cat << "Welcome-message"
 
  _____      _   _   _                                             _                                      _
@@ -43,7 +55,15 @@ else
   echo "Loading LogParser development environment"
 fi
 
-${DOCKER_BUILD} -t ${PROJECTNAME}-${OS} docker/${OS}
+${DOCKER_BUILD} -t ${PROJECTNAME}-devtools devtools/docker/
+
+buildStatus=$?
+
+if [ ${buildStatus} -ne 0 ];
+then
+    echo "Building the docker image failed."
+    exit ${buildStatus}
+fi
 
 if [ "$(uname -s)" == "Linux" ]; then
   USER_NAME=${SUDO_USER:=${USER}}
@@ -55,33 +75,93 @@ else # boot2docker uid and gid
   GROUP_ID=50
 fi
 
-${DOCKER_BUILD} -t "${PROJECTNAME}-${OS}-${USER_NAME}" - <<UserSpecificDocker
-FROM ${PROJECTNAME}-${OS}
-RUN bash /scripts/configure-for-user.sh "${USER_NAME}" "${USER_ID}" "${GROUP_ID}" "$(fgrep vboxsf /etc/group)"
+EXTRA_DOCKER_STEPS=""
+
+if [ -f "${HOME}/.gitconfig" ];
+then
+  cp "${HOME}/.gitconfig" ___git_config_for_docker
+  EXTRA_DOCKER_STEPS="ADD ___git_config_for_docker /home/${USER}/.gitconfig"
+fi
+
+DOCKER_GROUP_ID=$(getent group docker | cut -d':' -f3)
+
+cat - > ___UserSpecificDockerfile << UserSpecificDocker
+FROM ${PROJECTNAME}-devtools
+RUN bash /scripts/configure-for-user.sh "${USER_NAME}" "${USER_ID}" "${GROUP_ID}" "$(grep -F vboxsf /etc/group)"
+#RUN groupmod -g ${DOCKER_GROUP_ID} docker
+${EXTRA_DOCKER_STEPS}
 UserSpecificDocker
 
-# Do NOT Map the real ~/.m2 directory !!!
-[ -d "${PWD}/docker/_m2"    ] || mkdir "${PWD}/docker/_m2"
-[ -d "${PWD}/docker/_gnupg" ] || mkdir "${PWD}/docker/_gnupg"
+${DOCKER_BUILD} -t "${PROJECTNAME}-devtools-${USER_NAME}" -f ___UserSpecificDockerfile .
 
-MOUNTGPGDIR="${PWD}/docker/_gnupg"
+buildStatus=$?
+
+if [ ${buildStatus} -ne 0 ];
+then
+    echo "Building the user specific docker image failed."
+    exit ${buildStatus}
+fi
+
+rm -f ___git_config_for_docker ___UserSpecificDockerfile
+
+echo ""
+echo "Docker image build completed."
+echo "=============================================================================================="
+echo ""
+
+# Do NOT Map the real ~/.m2 directory !!!
+[ -d "${PWD}/devtools/docker/_m2"    ] || mkdir "${PWD}/devtools/docker/_m2"
+[ -d "${PWD}/devtools/docker/_gnupg" ] || mkdir "${PWD}/devtools/docker/_gnupg"
+
+MOUNTGPGDIR="${PWD}/devtools/docker/_gnupg"
 
 if [[ "${1}" == "RELEASE" ]];
 then
-  cp "${HOME}"/.m2/*.xml "${PWD}/docker/_m2"
+  cp "${HOME}"/.m2/*.xml "${PWD}/devtools/docker/_m2"
   MOUNTGPGDIR="${HOME}/.gnupg"
 
   echo "Setting up for release process"
 fi
 
-docker run --rm=true -t -i                                      \
-           -u "${USER_NAME}"                                    \
-           -v "${PWD}:/home/${USER_NAME}/${PROJECTNAME}"        \
-           -v "${PWD}/docker/_m2:/home/${USER_NAME}/.m2"        \
-           -v "${MOUNTGPGDIR}:/home/${USER_NAME}/.gnupg"  \
-           -w "/home/${USER}/${PROJECTNAME}"                    \
-           --name "${CONTAINER_NAME}"                           \
-           "${PROJECTNAME}-${OS}-${USER_NAME}"                  \
-           bash
+DOCKER_INTERACTIVE_RUN=${DOCKER_INTERACTIVE_RUN-"-i -t"}
+
+DOCKER_SOCKET_MOUNT=""
+if [ -S /var/run/docker.sock ];
+then
+  DOCKER_SOCKET_MOUNT="-v /var/run/docker.sock:/var/run/docker.sock${V_OPTS:-}"
+  echo "Enabling Docker support with the docker build environment."
+else
+  echo "There is NO Docker support with the docker build environment."
+fi
+
+COMMAND=( "$@" )
+if [ $# -eq 0 ];
+then
+  COMMAND=( "bash" "-i" )
+  DOCKER_INTERACTIVE="-i -t"
+else
+  DOCKER_INTERACTIVE="-i"
+fi
+
+# man docker-run
+# When using SELinux, mounted directories may not be accessible
+# to the container. To work around this, with Docker prior to 1.7
+# one needs to run the "chcon -Rt svirt_sandbox_file_t" command on
+# the directories. With Docker 1.7 and later the z mount option
+# does this automatically.
+# Since Docker 1.7 was release 5 years ago we only support 1.7 and newer.
+V_OPTS=:z
+
+docker run --rm=true ${DOCKER_INTERACTIVE}                      \
+       -u "${USER_NAME}"                                        \
+       -v "${PWD}:/home/${USER_NAME}/${PROJECTNAME}${V_OPTS:-}" \
+       -v "${HOME}/.m2:/home/${USER_NAME}/.m2${V_OPTS:-}"       \
+       -v "${MOUNTGPGDIR}:/home/${USER_NAME}/.gnupg${V_OPTS:-}" \
+       ${DOCKER_SOCKET_MOUNT}                                   \
+       -w "/home/${USER}/${PROJECTNAME}"                        \
+       -p 1313:1313                                             \
+       --name "${CONTAINER_NAME}"                               \
+       "${PROJECTNAME}-devtools-${USER_NAME}"                   \
+       "${COMMAND[@]}"
 
 exit 0
