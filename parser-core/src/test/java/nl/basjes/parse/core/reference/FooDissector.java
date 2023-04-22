034cbab92c2de36143316ca8d5c67545473e226d
Niels Basjes <niels@basjes.nl>
2023-04-22 15:59:58 +0200

    docs: Update copyright year.

--- parser-core/src/test/java/nl/basjes/parse/core/reference/FooDissector.java
+++ parser-core/src/test/java/nl/basjes/parse/core/reference/FooDissector.java
@@ -1,6 +1,6 @@
 /*
  * Apache HTTPD & NGINX Access log parsing made easy
- * Copyright (C) 2011-2021 Niels Basjes
+ * Copyright (C) 2011-2023 Niels Basjes
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
